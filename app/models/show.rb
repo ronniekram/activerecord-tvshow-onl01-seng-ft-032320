fcaba137d66120a692b57b88dc9014d328f593ba
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-27 00:11:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show
-    Show.where() 
+    Show.where("rating = ?", self.highest_rating).first 
   end 
   
   def self.lowest_rating
@@ -14,7 +14,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.least_popular_show
-    Show.minimum("rating").name 
+    Show.where("rating = ?", self.lowest_rating).first 
   end 
   
   def self.ratings_sum
